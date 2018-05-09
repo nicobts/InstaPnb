e00065ebc25bddea53432a53bcb97b48e513867f
Nicolas Bossi <nicolasbossi@gmail.com>
2018-05-09 14:11:49 +0200

    Update quickstartSKATE.py

--- quickstartSKATE.py
+++ quickstartSKATE.py
@@ -6,8 +6,8 @@ from selenium.common.exceptions import NoSuchElementException
 
 from instapy import InstaPy
 
-insta_username = 'dirtyrings'
-insta_password = 'troiachesei'
+insta_username = ''
+insta_password = ''
 
 # set headless_browser=True if you want to run InstaPy on a server
 
